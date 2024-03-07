5c91e3c127556a86904395e171760e5ea5ac114b
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-08 00:22:51 +0530

    INS

---
+++ src/main/java/InsertionSort/InsertionSort.java
@@ -0,0 +1,12 @@
+package InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int us_arr[] = { 4, 5, 1, 2, 3 };
+        System.out.println(Arrays.toString(us_arr));
+    }
+
+
+}
