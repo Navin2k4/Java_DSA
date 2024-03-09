937d4bdec79a260942cc786f6be6ab5b74d2a5f0
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-09 12:53:00 +0530

    textcommit

--- src/main/java/FrequencyCount.java
+++ src/main/java/FrequencyCount.java
@@ -1,25 +1,40 @@
 public class FrequencyCount {
     public static void main(String[] args) {
-        int arr[] = {1,2,2,3,1,4};
-        System.out.println(countfrequency(arr));
+        int arr[] = {1,2,3,4,5};
+        System.out.println(countFrequency(arr));
     }
-    static int countfrequency(int[] arr){
-        int freq = 1;
-        boolean nofreq = false;
-        for(int i=0;i<arr.length ; i++){
-            for (int j = 0 ; j< arr.length ; j++ ){
-                if(arr[i] == arr[j]){
-                    freq ++; 
-                    nofreq = true;
-                    break;
+
+    static int countFrequency(int[] arr) {
+        int maxFrequency = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int frequency = 1; 
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    frequency++;
                 }
             }
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+            }
         }
-        if(!nofreq){
-            return arr.length;
+
+        int countMaxFrequency = 0;
+        for (int num : arr) {
+            if (countFrequency(arr, num) == maxFrequency) {
+                countMaxFrequency++; 
+            }
         }
-        else{
-            return freq;
+
+        return countMaxFrequency;
+    }
+
+    static int countFrequency(int[] arr, int num) {
+        int count = 0;
+        for (int value : arr) {
+            if (value == num) {
+                count++; // Count occurrences of num in arr
+            }
         }
+        return count;
     }
 }
