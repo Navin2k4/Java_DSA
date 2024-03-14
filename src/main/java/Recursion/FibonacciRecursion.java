1a2331a7ebf4d734640c0aa755a397af02262328
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-14 22:09:28 +0530

    fibo updated

--- src/main/java/Recursion/FibonacciRecursion.java
+++ src/main/java/Recursion/FibonacciRecursion.java
@@ -3,13 +3,14 @@ package Recursion;
 public class FibonacciRecursion {
     public static void main(String[] args) {
         int n = 4;
-        System.out.println(fibo(n));
+        System.out.println("\nValue : "+fibo(n));
     }
     static int fibo(int n){
+        System.out.print(n + " ");
+
         if( n < 2 ){
             return n;
         }
-        System.out.print(n+" ");
         return fibo(n-1) + fibo(n-2);
     }
 }
