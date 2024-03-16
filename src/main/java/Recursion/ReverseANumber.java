7d043fc07f43c600267b2186a358a842ebce2a04
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-16 22:23:19 +0530

    Commited

--- src/main/java/Recursion/ReverseANumber.java
+++ src/main/java/Recursion/ReverseANumber.java
@@ -3,6 +3,7 @@ package Recursion;
 public class ReverseANumber {
     public static void main(String[] args) {
         int num = 12345;
+        System.out.println(revnumberusinghelper(num));
         revnum(num);
         System.out.println(sum);
     }
@@ -15,4 +16,18 @@ public class ReverseANumber {
         sum = sum * 10 +rem;
         revnum(num/10);
     }
+
+    static int revnumberusinghelper(int n) {
+        int digits = (int) (Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+
+    static int helper(int n, int digit) {
+        if (n == 0) {
+            return 0;
+        }
+        int rem = n % 10;
+        return rem * (int) Math.pow(10, digit - 1) + helper(n / 10, digit - 1);
+    }
+
 }
---
+++ target/classes/NumberSystemProgramming/basic.py
@@ -0,0 +1,6 @@
+print(12&10) # * AND Multiply 
+print(12|10) # * OR  Add
+print(12^10) # * XOR 1 true true else false
+print(~0)    # * Negotion of the value but incremented 
+print(12<<1) # * a << b = 2 * a (or) 2 * 2 raise to power b
+print(12>>1) # * a >> b = 2 / a (or) 2 / 2 raise to power b
