0a8e567744d647e2a00a6b9a2a6c7f6f9cad0478
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-20 20:44:15 +0530

    cc

--- src/main/java/Recursion/Arrays/LinearSearchRecursion.java
+++ src/main/java/Recursion/Arrays/LinearSearchRecursion.java
@@ -6,12 +6,11 @@ public class LinearSearchRecursion {
     public static void main(String[] args) {
         int nums[] = { 2, 4, 3, 5, 5, 8 };
         System.out.println(FindIndex(nums, 3, 0));
-        // Return =s the first occurrence of the element
         System.out.println(LinearFindIndexFront(nums, 5, 0));
-        // Returns the last occurrence of the element
         System.out.println(LinearFindIndexLast(nums, 5, nums.length - 1));
         findAllIndex(nums, 5, 0);
         System.out.println(list);
+        System.out.println(findAllIndexAsArray(nums, 5, 0, new ArrayList<>()));
     }
 
     static boolean FindIndex(int[] arr, int target, int index) {
@@ -54,6 +53,16 @@ public class LinearSearchRecursion {
             list.add(index);
         }
         findAllIndex(arr, target, index + 1);
+    }
+
+    static ArrayList<Integer> findAllIndexAsArray(int[] arr, int target, int index, ArrayList<Integer> arrlist) {
+        if (index == arr.length) {
+            return arrlist;
+        }
+        if (arr[index] == target) {
+            arrlist.add(index);
+        }
+        return findAllIndexAsArray(arr, target, index + 1, arrlist);
     }
 
 }
