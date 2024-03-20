82e3f6b6c3fc7615484f3d53abc6b03335789a10
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-20 20:35:30 +0530

    linearduplicatevalueindex

--- src/main/java/Recursion/Arrays/LinearSearchRecursion.java
+++ src/main/java/Recursion/Arrays/LinearSearchRecursion.java
@@ -1,36 +1,38 @@
 package Recursion.Arrays;
 
+import java.util.ArrayList;
+
 public class LinearSearchRecursion {
     public static void main(String[] args) {
-        int nums[] = {2,4,3,5,5,8};
-        System.out.println(LinearRecursion(nums, 3, 0));
-        // Return =s the first occourence of the element
+        int nums[] = { 2, 4, 3, 5, 5, 8 };
+        System.out.println(FindIndex(nums, 3, 0));
+        // Return =s the first occurrence of the element
         System.out.println(LinearFindIndexFront(nums, 5, 0));
-        // Returns the last occourence of the element
-        System.out.println(LinearFindIndexLast(nums, 5, nums.length-1));
-
+        // Returns the last occurrence of the element
+        System.out.println(LinearFindIndexLast(nums, 5, nums.length - 1));
+        findAllIndex(nums, 5, 0);
+        System.out.println(list);
     }
-    
-    static boolean LinearRecursion(int[] arr,int target,int index){
 
-        if(index == arr.length){
+    static boolean FindIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
             return false;
         }
-        return arr[index]==target || LinearRecursion(arr, target, index+1);
+        return arr[index] == target || FindIndex(arr, target, index + 1);
 
     }
-    
-    static int LinearFindIndexFront(int[] arr, int target,int index){
-        if(index == arr.length){
+
+    static int LinearFindIndexFront(int[] arr, int target, int index) {
+        if (index == arr.length) {
             return -1;
         }
-        if( arr[index] == target){
+        if (arr[index] == target) {
             return index;
         } else {
             return LinearFindIndexFront(arr, target, index + 1);
         }
     }
-    
+
     static int LinearFindIndexLast(int[] arr, int target, int index) {
         if (index == -1) {
             return -1;
@@ -41,4 +43,17 @@ public class LinearSearchRecursion {
             return LinearFindIndexLast(arr, target, index - 1);
         }
     }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
+
 }
--- src/main/java/Recursion/Arrays/SortedOrNotRecursion.java
+++ src/main/java/Recursion/Arrays/SortedOrNotRecursion.java
@@ -2,13 +2,14 @@ package Recursion.Arrays;
 
 public class SortedOrNotRecursion {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,5,6};
+        int arr[] = { 1, 2, 3, 5, 6 };
         System.out.println(isSorted(arr, 0));
     }
-    static boolean isSorted(int[] arr,int index){
-        if(index==arr.length-1){
+    
+    static boolean isSorted(int[] arr, int index) {
+        if (index == arr.length - 1) {
             return true;
         }
-        return arr[index] < arr[index+1] && isSorted(arr, index+1);
+        return arr[index] < arr[index + 1] && isSorted(arr, index + 1);
     }
 }
