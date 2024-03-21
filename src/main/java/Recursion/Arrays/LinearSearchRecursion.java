3502d97bb1ebbf6286e36ce7f1a8c1555756e458
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-21 19:34:57 +0530

    recursion day 4

--- src/main/java/Recursion/Arrays/LinearSearchRecursion.java
+++ src/main/java/Recursion/Arrays/LinearSearchRecursion.java
@@ -9,10 +9,21 @@ public class LinearSearchRecursion {
         System.out.println(LinearFindIndexFront(nums, 5, 0));
         System.out.println(LinearFindIndexLast(nums, 5, nums.length - 1));
         findAllIndex(nums, 5, 0);
+
+        // ! The list returned here is creeated outside the method using static
+        // variable.
         System.out.println(list);
-        System.out.println(findAllIndexAsArray(nums, 5, 0, new ArrayList<>()));
+
+        // ! This list is created at the main class and the function references the list
+        // ! and dosent generatr a new list
+        ArrayList<Integer> newlist = new ArrayList<>();
+        System.out.println(findAllIndexAsArray(nums, 5, 0, newlist));
+        
+        System.out.println(findAllIndexAsArray2(nums, 5, 0));
+
     }
 
+    // ? Returning the boolean weather the value is in the array or not
     static boolean FindIndex(int[] arr, int target, int index) {
         if (index == arr.length) {
             return false;
@@ -21,6 +32,7 @@ public class LinearSearchRecursion {
 
     }
 
+    // ? Returning the index of the element
     static int LinearFindIndexFront(int[] arr, int target, int index) {
         if (index == arr.length) {
             return -1;
@@ -32,6 +44,7 @@ public class LinearSearchRecursion {
         }
     }
 
+    // ? Returning the last index of the element
     static int LinearFindIndexLast(int[] arr, int target, int index) {
         if (index == -1) {
             return -1;
@@ -43,6 +56,7 @@ public class LinearSearchRecursion {
         }
     }
 
+    // ? Returning the index as an arraylist
     static ArrayList<Integer> list = new ArrayList<>();
 
     static void findAllIndex(int[] arr, int target, int index) {
@@ -55,6 +69,7 @@ public class LinearSearchRecursion {
         findAllIndex(arr, target, index + 1);
     }
 
+    // ? Passing the array list as the parameter
     static ArrayList<Integer> findAllIndexAsArray(int[] arr, int target, int index, ArrayList<Integer> arrlist) {
         if (index == arr.length) {
             return arrlist;
@@ -65,4 +80,18 @@ public class LinearSearchRecursion {
         return findAllIndexAsArray(arr, target, index + 1, arrlist);
     }
 
+    // ? using arraylist and not passing it into the argument and also using them
+    // ? inside the funciton
+    static ArrayList<Integer> findAllIndexAsArray2(int[] arr, int target, int index) {
+        ArrayList<Integer> newlist = new ArrayList<>();
+        if (index == arr.length) {
+            return newlist;
+        }
+        if (arr[index] == target) {
+            newlist.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = findAllIndexAsArray2(arr, target, index + 1);
+        newlist.addAll(ansFromBelowCalls);
+        return newlist;
+    }
 }
---
+++ src/main/java/Recursion/Arrays/RotatedBinarySearchRecursion.java
@@ -0,0 +1,5 @@
+package Recursion.Arrays;
+
+public class RotatedBinarySearchRecursion {
+    
+}
